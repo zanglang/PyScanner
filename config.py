06971e735f6def2c9d11cc010c941de8de856370
Jerry Chong <zanglang@gmail.com>
2007-10-13 10:25:55 +0000

    Initial import.

---
+++ config.py
@@ -0,0 +1,10 @@
+__doc__ = """
+Scanner configuration file
+version 0.1 - Jerry Chong <zanglang@gmail.com>
+"""
+
+# Google maps database path
+MapsPath = '.gmaps.db'
+
+# GPS device address for GPSd
+GpsdAddress = '/dev/rfcomm0'
---
+++ main.py
@@ -0,0 +1,8 @@
+__doc__ = """
+Scanner main module 
+version 0.1 - Jerry Chong <zanglang@gmail.com>
+"""
+
+# start scanner
+# read config file
+# initialize backends
